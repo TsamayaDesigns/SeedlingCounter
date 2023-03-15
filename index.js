924732a1c4daf53e3a3d5a9a32382b186cef815d
Eugene Gerber <eugene@tsamaya.co.uk>
2023-03-15 08:35:08 +0000

    PassengerCounter - Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+// document.getElementById("count").innerText = 5
+
+// let count = 5
+// count = count + 1
+// console.log(count)
+
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70
+// Console.log the value after each step
+
+// let bonusPoints = 50
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints + 50
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints - 75
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints + 45
+// console.log(bonusPoints)
+
+
+// PseudoCode
+// Initialise the count as 0
+// Listen for clicks on the increment button
+// Increment the count variable when button is clicked
+// Change the id=count-el in HTML to reflect the new count
+let count = 0
+
+function increment() {
+    console.log("Button was clicked")
+}
+
+
