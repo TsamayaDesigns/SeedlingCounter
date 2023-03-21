faaba8322e29c50b0405fa1f7a994ad122266df2
Eugene Gerber <eugene@tsamaya.co.uk>
2023-03-21 10:04:36 +0000

    Completed seedling counter

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
-// document.getElementById("count").innerText = 5
 
-// let count = 5
-// count = count + 1
-// console.log(count)
-
-// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
-// Decrease it down to 25, and then finally increase it to 70
-// Console.log the value after each step
-
-// let bonusPoints = 50
-// console.log(bonusPoints)
-
-// bonusPoints = bonusPoints + 50
-// console.log(bonusPoints)
-
-// bonusPoints = bonusPoints - 75
-// console.log(bonusPoints)
-
-// bonusPoints = bonusPoints + 45
-// console.log(bonusPoints)
-
-
-// PseudoCode
-// Initialise the count as 0
-// Listen for clicks on the increment button
-// Increment the count variable when button is clicked
-// Change the id=count-el in HTML to reflect the new count
+// Variables
 let countEl = document.getElementById("count-el");
-let count = 0
+let saveEl = document.getElementById("save-el");
+let totalEl = document.getElementById("total-el");
+let count = 0;
+let total = 0;
 
+// Increment & display the count
 function increment() {
-    count = count + 1;
-    // countEl.innerHTML = count;
-    countEl.innerText = count;
-    // countEl.textContent = count;
+    count += 1;
+    countEl.textContent = count;
 }
 
+// Save function to log out the count
+function save() {
+    
+    // Variable to contain the count and the dash separator, i.e. "12 - "
+    let countStr = count + " - ";
 
+    // Display the variable in the saveEl
+    saveEl.textContent += countStr;
+
+    // Calculate & Display the total number of seedlings
+    total += count;
+    totalEl.innerText = total;
+
+    // Reset counter after Save
+    count = 0;
+    countEl.textContent = 0;
+}
+
+// Reset function to reset everything
+function reset() {
+    count = 0;
+    countEl.textContent = 0;
+    saveEl.textContent = 0;
+    totalEl.textContent = "";
+    total = 0;
+    saveEl.innerHTML = "Previous entries:  ";
+}
